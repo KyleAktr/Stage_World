7eafe9b833d53f6aee10a7a3fe67b419b5610e59
Camille <camillepayen35@gmail.com>
2025-02-11 20:52:08 +0100

    Add date and location

--- api/artiste.go
+++ api/artiste.go
@@ -1,16 +1,52 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 type ArtistPageData struct {
-	Name           string
+	Name            string
 	SpotifyArtistID string
-	CreationDate   int
-	Members        []string
-	Image          string
+	CreationDate    int
+	Members         []string
+	Image           string
+	Concerts        []Concert
+}
+
+type Concert struct {
+	Location string
+	Date     string
+}
+
+type Relations struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+// fetchConcerts récupère les dates de concert pour un artiste donné
+func fetchConcerts(artistID int) (Relations, error) {
+	var relations Relations
+	relationsURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/relation/%d", artistID)
+
+	resp, err := http.Get(relationsURL)
+	if err != nil {
+		return relations, fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return relations, fmt.Errorf("API returned status code: %d", resp.StatusCode)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&relations)
+	if err != nil {
+		return relations, fmt.Errorf("error decoding response: %v", err)
+	}
+
+	return relations, nil
 }
 
 // Handler pour la page artiste
@@ -45,6 +81,24 @@ func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Récupérer les concerts
+	relations, err := fetchConcerts(artistData.ID)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to fetch concert data: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Créer la liste des concerts
+	var concerts []Concert
+	for location, dates := range relations.DatesLocations {
+		for _, date := range dates {
+			concerts = append(concerts, Concert{
+				Location: location,
+				Date:     date,
+			})
+		}
+	}
+
 	// Récupération du token Spotify
 	token, err := GetSpotifyToken()
 	if err != nil {
@@ -61,11 +115,12 @@ func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Préparer les données pour le template
 	data := ArtistPageData{
-		Name:           artistData.Name,
+		Name:            artistData.Name,
 		SpotifyArtistID: spotifyID,
-		CreationDate:   artistData.CreationDate,
-		Members:        artistData.Members,
-		Image:          artistData.Image,
+		CreationDate:    artistData.CreationDate,
+		Members:         artistData.Members,
+		Image:           artistData.Image,
+		Concerts:        concerts,
 	}
 
 	tmpl, err := template.ParseFiles("./html/artiste.html")
